50a82598f789daff1ecace3ed8b0c961d0c1ade4
ronald <ronaldkurouzo@gmail.com>
2024-06-29 07:34:32 -0300

    Java restful API
    Signed-off-by: ronald <ronaldkurouzo@gmail.com>

---
+++ src/test/java/com/ronald/projeto_dio_api_rest/ProjetoDioApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronald.projeto_dio_api_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDioApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
