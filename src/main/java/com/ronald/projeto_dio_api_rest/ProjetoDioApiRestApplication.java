344da05129ad2f57c209141442b1d4e7b6b099c0
Ronald Antonio Oliveira de sousa <91800966+Hidan404@users.noreply.github.com>
2024-06-29 07:57:10 -0300

    Add files via upload

---
+++ src/main/java/com/ronald/projeto_dio_api_rest/ProjetoDioApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.ronald.projeto_dio_api_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDioApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDioApiRestApplication.class, args);
+	}
+
+}
